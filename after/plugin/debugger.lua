de0605ef07b8d49ea0fca98393c46a55b11c030b
Matheus Rocha <01devmath@gmail.com>
2025-12-31 21:01:30 -0300

    feat: use vscode launch.json behavior

--- after/plugin/debugger.lua
+++ after/plugin/debugger.lua
@@ -6,3 +6,6 @@ require("dap-view").setup{}
 require("nvim-dap-virtual-text").setup({
     virt_text_pos = "eol",
 })
+
+require('dap.ext.vscode').json_decode = require'json5'.parse
+require('dap.ext.vscode').load_launchjs()
